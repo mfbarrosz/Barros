64bb366e5df824262f7a453844065d09a1be495f
mfbarrosz <166130226+mfbarrosz@users.noreply.github.com>
2024-04-11 08:51:31 -0300

    Create questao7.py

---
+++ estruturasequencial/questao7.py
@@ -0,0 +1,7 @@
+#7 - Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+lado = float(input("Digite o lado do quadrado: "))
+area_quadrado = lado**2
+dobro_area = 2 * area_quadrado
+
+print(f"A área do quadrado é: {area_quadrado}")
+print(f"O dobro da área do quadrado é: {dobro_area}")
