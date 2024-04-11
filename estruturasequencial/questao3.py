b440988c80bde9105b14463aa5592ba341dfb205
mfbarrosz <166130226+mfbarrosz@users.noreply.github.com>
2024-04-11 08:38:08 -0300

    Create questao3.py

---
+++ estruturasequencial/questao3.py
@@ -0,0 +1,5 @@
+3 - Faça um Programa que peça dois números e imprima a soma.
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+soma = numero1 + numero2
+print("A soma dos números é:", soma)
