d928feb8cba77bacbc186a3305a9e6c3c498958f
mfbarrosz <166130226+mfbarrosz@users.noreply.github.com>
2024-04-11 09:02:53 -0300

    Create questao9.py

---
+++ estruturasequencial/questao9.py
@@ -0,0 +1,7 @@
+9 - Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius. C = 5 * ((F-32) / 9).
+
+fahrenheit = float(input("Digite a temperatura em graus Fahrenheit: "))
+celsius = 5 * ((fahrenheit - 32) / 9)
+
+print(f"A temperatura em graus Celsius é:", celsius)
+
